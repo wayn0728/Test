382efc821c9edf3008c6343af239294711ae285e
Jiawei Dong <jwdong890728@gmail.com>
2012-11-07 12:23:19 -0500

    test

--- TestForGit/src/test.java
+++ TestForGit/src/test.java
@@ -2,6 +2,8 @@
 public class test {
 	public static void main(String[] args) {
 		System.out.print("Hello World!");
+		System.out.print("Forest Did!");
+		System.out.print("wayne!");
 		System.out.print("wayne!");
 	}
 }
